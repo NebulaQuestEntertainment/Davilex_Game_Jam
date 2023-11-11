410024f3e8e05fab0516c973051f30fd7b31acff
NebulaQuestEntertainment <150523579+NebulaQuestEntertainment@users.noreply.github.com>
2023-11-11 19:00:53 +0100

    gun system

---
+++ amsterDoom clone/Assets/Scripts/GunSystem.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class GunSystem : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int damage;
+    public float timeBetweenShooting, spread, range, reloadTime, timeBetweenShots;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+    bool shooting, readyToShoot, reloading;
+
+    public Camera fpsCam;
+    public Transform attackPoint;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+
+    public GameObject muzzleFlash, bulletHoleGraphic;
+    public CamShake camShake;
+    public float camShakeMagnitude, camShakeDuration;
+    public TextMeshProUGUI text;
+
+    void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        MyInput();
+        text.SetText(bulletsLeft + " / " + magazineSize);
+    }
+    private void MyInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+    
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        float newSpread = spread;
+        if (rb.velocity.magnitude > 0)
+            spread = newSpread * 1.5f;
+        else spread = newSpread;
+
+        //calculate direction with spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x, y, 0);
+
+        //raycast
+        if (Physics.Raycast(fpsCam.transform.position, direction, out rayHit, range, whatIsEnemy))
+        {
+            Debug.Log(rayHit.collider.name);
+
+            //if (rayHit.collider.CompareTag("Enemy"))
+            //    rayHit.collider.GetComponent<ShootingAi>().TakeDamage(damage);
+        }
+
+        camShake.Shake(camShakeDuration, camShakeDuration);
+
+        Instantiate(bulletHoleGraphic, rayHit.point, Quaternion.Euler(0, 180, 0));
+        Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+
+        bulletsLeft--;
+        bulletsShot--;
+
+        Invoke("ResetShot", timeBetweenShooting);
+
+        if (bulletsShot > 0 && bulletsLeft > 0)
+        Invoke("Shoot", timeBetweenShots);
+    }
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+    }
+    private void  ReloadFinished() 
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
